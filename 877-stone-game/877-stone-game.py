3ef7bc3601583a521ced84f720d90a7ccb367f02
Melvin J Joseph <102798833+melvinjjoseph@users.noreply.github.com>
2023-05-10 12:54:26 +0530

    Time: 55 ms (48.54%), Space: 16.4 MB (45.80%) - LeetHub

---
+++ 877-stone-game/877-stone-game.py
@@ -0,0 +1,21 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        # alice=0
+        # bob=0
+        # while(len(piles)>0):
+        #     if piles[0]>piles[-1]:
+        #         alice+=piles[0]
+        #         piles.pop(0)
+        #     else:
+        #         alice+=piles[-1]
+        #         piles.pop()
+        #     if piles[0]>piles[-1]:
+        #         bob+=piles[0]
+        #         piles.pop(0)
+        #     else:
+        #         bob+=piles[-1]
+        #         piles.pop()
+        # if alice>bob:
+        #     return True
+        # return False
+        return True
