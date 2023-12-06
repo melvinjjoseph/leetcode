791a6c0c8ef43a89c4c4e38ec0dc6c09589a911d
Melvin J Joseph <102798833+melvinjjoseph@users.noreply.github.com>
2023-12-06 13:41:29 +0530

    Time: 30 ms (97.24%), Space: 16.3 MB (40.24%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -0,0 +1,15 @@
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        if n<8:
+            return n*(n+1)//2
+        k=n//7
+        F=28
+        L = 28 + (k - 1) * 7
+        arithmetic_sum = k * (F + L) // 2
+        
+        monday = 1 + k
+        final_week = 0
+        for day in range(n % 7):
+            final_week += monday + day
+        
+        return arithmetic_sum + final_week
