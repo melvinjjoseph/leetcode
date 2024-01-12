f40eceabe6d4f5d8258b7a4580f32100cd51b871
Melvin J Joseph <102798833+melvinjjoseph@users.noreply.github.com>
2024-01-12 12:33:55 +0530

    Time: 35 ms (90.17%), Space: 17.4 MB (9.07%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
@@ -1,23 +1,14 @@
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
-        a=s[0:len(s)//2]
-        b=s[len(s)//2:len(s)]
-        #print(a,b)
-        vowels=['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-        a_vowels=0
-        b_vowels=0
-        a_conso=0
-        b_conso=0
-        for i in range(len(s)//2):
-            if a[i] in vowels:
-                a_vowels=a_vowels+1
-            else:
-                a_conso=a_conso+1
-            if b[i] in vowels:
-                b_vowels=b_vowels+1
-            else:
-                b_conso=b_conso+1
-        if a_vowels==b_vowels and a_conso==b_conso:
-            return True
-        else: 
-            return False
+        vowels=['a','A','e','E','i','I','o','O','u','U']
+        a=s[:len(s)//2]
+        b=s[len(s)//2:]
+        c1=0
+        c2=0
+        for i in a:
+            if i in vowels:
+                c1+=1
+        for i in b:
+            if i in vowels:
+                c2+=1
+        return c1==c2
