1bcc1da7c4a040a5d860dbd85f268b16f221f82e
Melvin J Joseph <102798833+melvinjjoseph@users.noreply.github.com>
2024-05-31 11:56:06 +0530

    Time: 59 ms (53.23%), Space: 18.7 MB (30.48%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        once=set()
+        for num in nums:
+            if num in once:
+                once.remove(num)
+            else:
+                once.add(num)
+        return list(once)
