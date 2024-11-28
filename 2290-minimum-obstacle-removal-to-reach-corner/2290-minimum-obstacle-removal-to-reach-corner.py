d876eedc7ba4296b96ce4f9c080c62a4a37dacbf
Melvin J Joseph <102798833+melvinjjoseph@users.noreply.github.com>
2024-11-28 23:06:07 +0530

    Time: 833 ms (92.59%), Space: 47 MB (26.87%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
@@ -2,27 +2,38 @@ class Solution:
     def minimumObstacles(self, grid: List[List[int]]) -> int:
         ROWS, COLS= len(grid), len(grid[0])
         
-        pq=[(0,0,0)]
+        dq=deque([(0,0,0)])
         
         visit=set([(0,0,0)])
         
-        while pq:
-            obs, r,c= heapq.heappop(pq)
+        while dq:
+            obs, r,c= dq.popleft()
             if r==ROWS-1 and c==COLS-1:
                 return obs
             if r+1<ROWS and (r+1,c) not in visit:
-                heapq.heappush(pq,(obs+grid[r+1][c],r+1,c))
                 visit.add((r+1,c))
+                if grid[r+1][c]:
+                    dq.append((1+obs,r+1,c))
+                else:
+                    dq.appendleft((obs,r+1,c))
             if r-1>=0 and (r-1,c) not in visit:
-                heapq.heappush(pq,(obs+grid[r-1][c],r-1,c))
                 visit.add((r-1,c))
+                if grid[r-1][c]:
+                    dq.append((1+obs,r-1,c))
+                else:
+                    dq.appendleft((obs,r-1,c))
             if c+1<COLS and (r,c+1) not in visit:
-                heapq.heappush(pq,(obs+grid[r][c+1],r,c+1))
                 visit.add((r,c+1))
+                if grid[r][c+1]:
+                    dq.append((1+obs,r,c+1))
+                else:
+                    dq.appendleft((obs,r,c+1))
             if c-1>=0 and (r,c-1) not in visit:
-                heapq.heappush(pq,(obs+grid[r][c-1],r,c-1))
                 visit.add((r,c-1))
-        
+                if grid[r][c-1]:
+                    dq.append((1+obs,r,c-1))
+                else:
+                    dq.appendleft((obs,r,c-1))
             
             
             
