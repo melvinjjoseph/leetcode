51cb05904e1147dd8e2e4cdeb28e6f4df5793eae
Melvin J Joseph <102798833+melvinjjoseph@users.noreply.github.com>
2024-12-12 12:55:18 +0530

    Time: 3 ms (87.72%), Space: 17.6 MB (8.46%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -1,8 +1,7 @@
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
-        max_heap=[]
-        for gift in gifts:
-            heapq.heappush(max_heap,-gift)
+        max_heap=[-gift for gift in gifts]
+        heapq.heapify(max_heap)
         for _ in range(k):
             maxi=-heapq.heappop(max_heap)
             maxi=maxi**0.5
